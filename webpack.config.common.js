f76139dc89bd10bf2bf15104cb393cd4654e9932
Venus <venusyip@outlook.com>
2024-05-22 03:56:49 +0100

    Add production files

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'docs'),
   },
   target: 'web',
   resolve: {
